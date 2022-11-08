10c7024314a95dc6144d67409c89b9d765da261b
Berkay Akbas <berkayakbas@gmail.com>
2022-11-08 15:22:47 +0300

    Fix linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :update_allowed_parameters, if: :devise_controller?
+
   protected
 
   def update_allowed_parameters
@@ -10,7 +11,8 @@ class ApplicationController < ActionController::Base
       u.permit(:name, :email, :password, :current_password)
     end
   end
-  def after_sign_in_path_for(resource)
+
+  def after_sign_in_path_for(_resource)
     root_path
   end
 end
