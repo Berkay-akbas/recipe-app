eb915deecc5a1deb809a6dba6a337a7c769fae71
Berkay Akbas <berkayakbas@gmail.com>
2022-11-09 17:57:40 +0300

    Fix linters

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,10 +3,10 @@ class RecipesController < ActionController::Base
     if current_user.nil?
       redirect_to new_user_session_path
     else
-    @user = current_user
-    @recipes = @user.recipes.all
+      @user = current_user
+      @recipes = @user.recipes.all
+    end
   end
-end
 
   def show
     @recipe = Recipe.find(params[:id])
