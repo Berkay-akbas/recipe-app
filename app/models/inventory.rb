88b58e7d4df88c05a0c620f624b5ac5a9c1c35c1
Roseokpe <roseokpe@gmail.com>
2022-11-08 18:49:19 -0800

    create inventory

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -2,4 +2,13 @@ class Inventory < ApplicationRecord
   belongs_to :user, foreign_key: :user_id, class_name: 'User'
   has_many :inventory_foods, foreign_key: :inventory_id, dependent: :destroy
   has_many :foods, through: :inventory_foods
+
+  
+  def recent_three_inventories
+    inventories.order(created_at: :desc).limit(3)
+  end
+
+  def admin?
+    role == 'admin'
+  end
 end
